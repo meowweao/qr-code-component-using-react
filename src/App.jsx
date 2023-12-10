dcdb11a819d2a4c8ac9927541aa2f85c877400c4
meowweao <ganeshfalak01@gmail.com>
2023-12-10 19:10:01 +0530

    Add QR code Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import './App.css'
+
 
 const App = () => {
   return (
-    <div>
-      <h1>My App</h1>
+    <div className='container'>
+
+      <img src="src\assets\image-qr-code.png" alt="" />
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      
     </div>
   )
 }
