d91231213651f412cfeae6cd9cbd97333b5a07fa
meowweao <ganeshfalak01@gmail.com>
2023-12-12 00:44:05 +0530

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ const App = () => {
   return (
     <div className='container'>
 
-      <img src="src\assets\image-qr-code.png" alt="" />
-      <h1>Improve your front-end skills by building projects</h1>
-      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-      
+      <div className="qr-code-component">
+        <img src="src\assets\image-qr-code.png" alt="" />
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+
     </div>
   )
 }
